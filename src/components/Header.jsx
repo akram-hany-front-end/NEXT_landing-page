2fc99ce239c83440b07f3b3d9e9cc47a12dfe052
akram-hany-front-end <akramhanyahmed@gmail.com>
2026-04-07 13:51:25 -0700

    header-finished

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="header-container">
+<h3 className="header-logo">realme GT6</h3>
+<div className="links-container-header">
+    <a href="#">AI Vision</a>
+    <a href="#">Performance</a>
+    <a href="#">Display</a>
+    <a href="#">Charging</a>
+</div>
+<button className="header-button">Buy Now</button>
+  </div>;
+};
+
+export default Header;
